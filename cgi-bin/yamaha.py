5fb13cca001f69161060e35883ee89b2dc9423cc
Milton Dorkenhoff <miltondorkenhoff@gmail.com>
2018-06-23 22:45:20 +0000

    add the zone name in the results.

--- cgi-bin/yamaha.py
+++ cgi-bin/yamaha.py
@@ -131,4 +131,4 @@ else:
     rc = getInfo( receiver, zone )
     # print >> sys.stderr, rc
     print "Content-type:application/json\r\n\r\n"
-    print "{ \"%s\" : [ { \"power\" : \"%s\" }, { \"source\" : \"%s\" } ] }" % ( zone, rc[ 0 ], rc [ 1 ] )
+    print "{ \"results\" : [ { \"name\" : \"%s\" }, { \"power\" : \"%s\" }, { \"source\" : \"%s\" } ] }" % ( zone, rc[ 0 ], rc [ 1 ] )
